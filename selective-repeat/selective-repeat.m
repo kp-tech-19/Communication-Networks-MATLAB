efcb8701febb599f111be1315bafbea739ae0e24
Priyadharshini K <priyadharshinik1901@gmail.com>
2025-12-01 19:32:32 +0530

    Update selective-repeat.m

--- selective-repeat/selective-repeat.m
+++ selective-repeat/selective-repeat.m
@@ -1,79 +1,76 @@
-clc;clear;
+clear; clc;
 
-framesize=input("Enter framesize:");
-m=input("Enter m value:");
-data='1010101000001110000010101010';
-totalframe=floor(length(data)/framesize);
-errorprb=0.2;
-windowsize=2^(m-1);
+errorprobability = 0.2;
+framesize = input("Enter frame size (data bits): ");
+m = input("Enter number of bits for sequence numbers: ");
+windowsize = 2^m - 1;
+data = '0101101110100000100101011011001011001001';
+totalframes = floor(length(data)/framesize);
+ack = zeros(1, totalframes);
+frame = 1;
 
-ack=zeros(1,totalframe);
-frame=1;
-
-while frame<=totalframe
-     windowend=min(windowsize+frame-1,totalframe);
-     fprintf("Sending frame from %d to %d:\n",frame-1,windowend-1);
-     failed=[];
-
-      
-     
-     for f=frame:windowend
-         event=rand();
-         if ack(f)==1
-             continue;
-         end
-         startidx = (f - 1) * framesize + 1; %9
-         endidx=startidx + framesize - 1;
-         val = data(startidx : endidx);
-         if event<0.2
-             fprintf("Reciver:Frame %d lost.Retransmitting frame %d.....\n",f-1,f-1);
-             failed(end+1)=f;
-         elseif event<0.4
-             fprintf("Reciver:Frame %d recived.Ack lost.\n",f-1);
-             failed(end+1)=f;
-         elseif event<0.6
-             fprintf("Reciver:Frame %d recived.Ack delayed.\n",f-1);
-             failed(end+1)=f;
-         else
-            if rand<errorprb
-               index = randi([1, framesize]);
-                if val(index) == '0'
-                    val(index) = '1';
-                else
-                    val(index) = '0';
-                end
-                 fprintf('Frame %d corrupted during transmission!\n', f-1);
-                 fprintf('Frame %d received: %s\n', f-1, val);
-                 if strcmp(val, data(startidx:endidx))
-                     ack(f)=1;
-                    fprintf('Receiver: Frame %d received correctly. ACK %d sent.\n', f-1, f-1);
-                else
-                    fprintf('Receiver: Frame %d corrupted! No ACK.\n', f-1);
-                    fprintf('Sender: Timeout! Restarting from Frame %d\n', f-1);
-                    failed(end+1)=f;
-                end
-            else
-                fprintf("Receiver: Frame %d received correctly. ACK sent.\n", f-1);
-                ack(f) = 1;
-            end
+while frame <= totalframes
+    windowend = min(frame + windowsize - 1, totalframes);
+    fprintf('Sending frames %d to %d...\n', frame - 1, windowend - 1);
+    failed = [];
     
-         end
-     end
-     for f=failed
-            fprintf("Retransmitting the frame %d.....\n",f-1);
-            while true
-                if rand<errorprb
-                    fprintf("Retransmitting the frame %d failed.....\n",f-1);
-                    failed(end+1)=f;
-                else
-                    fprintf("Retransmitting the frame %d successfully....\n",f-1);
-                    ack(f)=1;break;
-                end
+    for f = frame:windowend
+        if ack(f) == 1
+            continue;
+        end
+        
+        startidx = (f - 1)*framesize + 1;
+        endidx = startidx + framesize - 1;
+        val = data(startidx:endidx);
+        event = rand();
+        
+        if event < 0.2
+            fprintf('Frame %d corrupted.\n', f-1);
+            failed(end+1) = f;
+        elseif event < 0.4
+            fprintf('Receiver: Frame %d received but ACK lost!\n', f-1);
+            failed(end+1) = f;
+        elseif event < 0.6
+            fprintf('Receiver: Frame %d received but ACK delayed!\n', f-1);
+            failed(end+1) = f;
+        else
+            if rand < errorprobability
+                idx = randi([1, framesize]);
+                val(idx) = char('0' + '1' - val(idx));
+                fprintf('Frame %d corrupted during transmission!\n', f-1);
             end
-            
+            fprintf('Frame %d received: %s\n', f-1, val);
+            original_val = data(startidx:endidx);
+            if strcmp(val, original_val)
+                ack(f) = 1;
+                fprintf('Receiver: Frame %d received. ACK sent.\n', f-1);
+            else
+                fprintf('Frame %d corrupted.\n', f-1);
+                failed(end+1) = f;
+            end
+        end
+        
+        pause(0.3);
     end
-    while frame<=totalframe && ack(frame)==1
-                 frame=frame+1;
+    
+    for f = failed
+        if ack(f) == 1
+            continue;
+        end
+        fprintf('Retransmitting Frame %d...\n', f-1);
+        while ack(f) == 0
+            if rand < errorprobability
+                fprintf('Retransmission of Frame %d failed.\n', f-1);
+            else
+                ack(f) = 1;
+                fprintf('Retransmission of Frame %d successful.\n', f-1);
+            end
+        end
+    end
+    
+    while frame <= totalframes && ack(frame) == 1
+        frame = frame + 1;
     end
 end
-fprintf("All frames are transmitted successfully.\n");
+
+fprintf('\nAll frames successfully transmitted and acknowledged.\n');
