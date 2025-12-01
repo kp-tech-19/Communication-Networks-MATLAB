8b1f2d35e55064d4aac7666ba002144ab4d7e59c
Priyadharshini K <priyadharshinik1901@gmail.com>
2025-12-01 18:04:54 +0530

    Update stop-and-wait.m

--- stop-and-wait/stop-and-wait.m
+++ stop-and-wait/stop-and-wait.m
@@ -1,67 +1,61 @@
 clc;clear;
-framesize = input("enter the framesize:");
-data = '1010101001010101001100100';
-totalframes = floor(length(data)/framesize);
-errorprob = 0.2;
 
-ack = zeros([1, totalframe]);lost = 0;frame = 1;
+framesize=input ("Enter framesize:");
+data='10101010110011010101001001001010';
+totalframes=floor(length(data)/framesize);
+errorprb=0.2;
 
-while frame <= totalframes
+ack=zeros([1,totalframes]);lost=0;frame=1;
 
-    frameno = mod(frame-1, 2);
-    ackno = mod(frame, 2);
+while frame<=totalframes
+    frameno=mod(frame-1,2);
+    ackno=mod(frame,2);
 
-    startidx = (frame-1)*framesize + 1;
-    endidx = startidx + framesize - 1;
-    val = data(startidx:endidx);
+    startidx=(frame-1)*framesize+1;
+    endidx=startidx+framesize-1;
+    val=data(startidx:endidx);
 
-    fprintf("transmitting frame %d\n", frameno);
-    fprintf("frame data %s\n", val);
+    fprintf("Transmitting frame %d with data %s\n",frameno,val);
+    cases=["no ack","ack","delayed ack"];
+    cases_str=cases(randi([1,3]));
 
-    cases = ["no ack", "ack", "delayed"];
-    case_str = cases(randi([1, 3]));
-
-    if case_str == "no ack"
-        fprintf("ack %d not received\n", ackno);
+    if cases_str=="no ack"
+        fprintf("Ack %d not recived\n",ackno);
         pause(1);
-        lost = lost + 1;
-        if lost == 5
-            fprintf("timeout. retransmitting frame...\n");
-            pause(1);
-            ack(frame) = ackno;frame = frame + 1; lost = 0;
-            fprintf("ack %d received\n", ackno);
+        lost=lost+1;
+        if lost==5
+           fprintf("Timeout,retransmitting frame....\n");
+           pause(1);
+           ack(frame)=ackno;lost=0;frame=frame+1;
+           fprintf("Ack %d recived \n",ackno);
         end
         continue;
-
-    elseif case_str == "ack"
-        if rand < errorprob
-            index = randi([1, framesize]);
-            if val(index) == '0'
-                val(index) = '1';
-            else
-                val(index) = '0';
+    elseif cases_str=="ack"
+        if rand< errorprb
+            index=randi([1,framesize]);
+            if val(index)=='0'
+                val(index)='1';
+            else 
+                val(index)='0';
             end
-            fprintf("frame is corrupted\n");
-            fprintf("received frame data %s\n", val);
-            original_val = data(startidx:endidx);
-            if strcmp(val, original_val)
-                fprintf("ack %d received\n", ackno);
-                ack(frame) = ackno;frame = frame + 1; lost = 0;
+            fprintf("Frame %d corruputed. Frame recived :%s\n",frameno,val);
+            original_val=data(startidx:endidx);
+            if strcmp(val,original_val)
+                fprintf("Ack %d recived ",ackno); 
+                ack(frame)=ackno;lost=0;frame=frame+1;
             else
-                fprintf("frame %d is corrupted. retransmitting data...\n", frame);
-                pause(1);
-                continue;
+               fprintf("Frame %d corruputed. Retransmitting frame....\n",frameno);
+               pause(1);
+               continue;
             end
         else
-            fprintf("ack %d received\n", ackno);
-            ack(frame) = ackno;frame = frame + 1;lost = 0;
+          fprintf("Ack %d recived\n ",ackno); 
+          ack(frame)=ackno;lost=0;frame=frame+1;
         end
-
-    elseif case_str == "delayed"
-        fprintf("ack %d received after delay\n", ackno);
-        ack(frame) = ackno;frame = frame + 1;lost = 0;
+        
+    elseif cases_str=="delayed ack"
+        fprintf("Ack %d recived after delay\n",ackno);
+        ack(frame)=ackno;lost=0;frame=frame+1;
     end
 end
-
-fprintf("all frames transmitted successfully\n");
-
+fprintf("All frames transmitted successfully\n");
